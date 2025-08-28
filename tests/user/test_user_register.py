7718d1a8b578a69ee1ac2acc1814d2c2130f0ee5
MrXian763 <xianzhiyong763@gmail.com>
2025-08-28 12:14:09 +0800

    优化用户模块注册接口，避免正向用例重复注册导致断言失败

--- tests/user/test_user_register.py
+++ tests/user/test_user_register.py
@@ -2,6 +2,8 @@ import pytest
 from utils.data_handler import data_handler
 from utils.logger import logger
 from utils.assertions import assertions
+import random
+import time
 
 
 def test_post_user_register_success(user_api):
@@ -10,7 +12,7 @@ def test_post_user_register_success(user_api):
     success_case = test_cases[0]
 
     logger.info(f"开始执行用例：{success_case['case_id']} - {success_case['title']}")
-    response = user_api.register(success_case['data']["checkPassword"], success_case['data']["userAccount"],
+    response = user_api.register(success_case['data']["checkPassword"], generate_unique_account(),
                                  success_case['data']["userPassword"])
 
     assertions.assert_response_code(response, success_case['expected_code'])
@@ -33,3 +35,15 @@ def test_post_user_register_fail(user_api, fail_case):
     assertions.assert_response_code(response, fail_case['expected_code'])
     assertions.assert_equal(err_msg, fail_case['expected_message'])
     assertions.assert_response_time(response, 1)
+
+
+def generate_unique_account():
+    """生成随机账号，避免正向用例预期错误"""
+    prefix = "test_"
+    timestamp = str(int(time.time()))[-6:]
+    random_num = random.randint(10, 99)
+    account = f"{prefix}{timestamp}_{random_num}"
+    return account
+
+
+"""todo 根据用户名删除用户，避免测试数据污染"""
